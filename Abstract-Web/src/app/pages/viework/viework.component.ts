ab4865a0dc302d8390fa69ab0551d99cd43f22ce
moranzargari <moran4151@gmail.com>
2018-10-16 12:11:18 +0300

    viework component

---
+++ Abstract-Web/src/app/pages/viework/viework.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viework',
+  templateUrl: './viework.component.html',
+  styleUrls: ['./viework.component.css']
+})
+export class VieworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
