ea97856a96b923113c9883ac270f0ed6569d9a6e
Itay-Hefetz <hefetz99@gmail.com>
2018-10-30 10:29:37 +0200

    update settings

--- Abstract-Web/src/environments/environment.prod.ts
+++ Abstract-Web/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCPcTGPUMRwZi8Ma3A1oam0xUhbnfD18FE",
+    authDomain: "abstract-a5553.firebaseapp.com",
+    databaseURL: "https://abstract-a5553.firebaseio.com",
+    projectId: "abstract-a5553",
+    storageBucket: "abstract-a5553.appspot.com",
+    messagingSenderId: "767610379326"
+  }
 };
